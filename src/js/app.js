9680ec792613cfe7929f1c1941f447e120732f8d
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2020-12-18 23:20:00 +0100

    Task 10.4 - to fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,62 @@
-import {settings,select} from './settings.js';
+import {settings,select,classNames} from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
+import Booking from './components/Booking.js';
 
 const app = {
+
+  initBooking: function(){
+    const thisApp = this;
+    thisApp.bookingWrapper = document.querySelector(select.containerOf.booking);
+    new Booking(thisApp.bookingWrapper);
+  },
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    const idFromHash = window.location.hash.replace('#/','');
+    let pageMatchingHash = thisApp.pages[0].id;
+    for (let page of thisApp.pages){
+      if(page.id === idFromHash){
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+    thisApp.activatePage(pageMatchingHash);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+        /* get pageId from attribute href */
+        const id = clickedElement.getAttribute('href').replace('#','');
+        /* run thisApp.activatePage with that id */
+        thisApp.activatePage(id);
+        /* change URL has */
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    /* add class 'actove' to matching pages, remove form non-matching*/
+
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active,page.id == pageId);
+    }
+    /* add class 'actove' to matching links, remove form non-matching*/
+
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+  },
+
   initData: function(){
     const thisApp = this;
 
@@ -13,14 +67,12 @@ const app = {
         return rawResponse.json();
       })
       .then(function(parsedResponse){
-        console.log('parsedResponse', parsedResponse);
 
         /* save parsedResponse as thisApp.data.products */
         thisApp.data.products = parsedResponse;
         /* execute initMenu method */
         thisApp.initMenu();
       });
-    console.log('thisApp.data', JSON.stringify(thisApp.data));
   },
 
   initMenu: function(){
@@ -44,11 +96,12 @@ const app = {
 
   init: function(){
     const thisApp = this;
-    console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
+    console.log('** App starting **');
 
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,6 +1,6 @@
 import {templates,select} from './../settings.js';
-//import utils from './../utils.js';
 import AmountWidget from './AmountWidget.js';
+
 class Booking{
   constructor(element){
     const thisBooking = this;
@@ -12,19 +12,23 @@ class Booking{
     const generatedHTML = templates.bookingWidget();
     thisBooking.dom = {};
     thisBooking.dom.wrapper = element;
-    thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
-    thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
-    console.log(thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount));
+    thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
+    thisBooking.dom.hoursAmount = thisBooking.dom.wrapper.querySelector(select.booking.hoursAmount);
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
 
   }
 
   initWidgets(){
     const thisBooking = this;
+    console.log(document.querySelector(select.booking.peopleAmount));
+    console.log(document.querySelector(select.booking.hoursAmount));
     thisBooking.peopleAmountWidget = new AmountWidget(thisBooking.dom.peopleAmount);
     thisBooking.dom.peopleAmount.addEventListener('click', function(){
     });
-    //thisBooking
+    thisBooking.aoursAmountWidget = new AmountWidget(thisBooking.dom.hoursAmount);
+    thisBooking.dom.hoursAmount.addEventListener('click', function(){
+
+    });
   }
 }
 
